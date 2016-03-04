270c1c4c44f3e2b0e305d60e7945d64e56c72fec
shawngchristy <shawn.g.christy@gmail.com>
2016-03-04 16:48:44 +0000

    add connections formula, distance, etc

---
+++ app/helpers/locations_helper.rb
@@ -0,0 +1,23 @@
+module LocationsHelper
+    #Parse the api data to store it in a Ruby array of hashes = each bus is a hash
+    def fetch_buses_from_api(source_url)
+        http = Net::HTTP.get_response(URI.parse(source_url))    
+        data = http.body
+        JSON.parse(data)
+    end
+    
+    #return true if a bus is nearby and false otherwise
+    def is_nearby?(user_lat, user_long, bus_lat, bus_long)
+        #max distance in degrees approx 0.6 miles
+        max_distance = 0.01
+        
+        difference_latitudes = user_lat - bus_lat.to_f
+        difference_longitudes = user_long - bus_long.to_f
+        
+        distance = Math.sqrt(difference_latitudes ** 2 + difference_longitudes ** 2)
+        
+        distance <= max_distance        
+    end
+    
+    
+end
